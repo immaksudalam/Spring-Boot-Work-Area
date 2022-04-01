989e14fe7e09eb6134864a456b0c1c8125462a76
immaksudalam <maksudalamrony56@gmail.com>
2022-04-01 02:30:34 -0700

    Tracking project finished

---
+++ Taking input and Showing it in thymeleaf page/src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/home")
+	public String homePage()
+	{
+		return "HomePage.html";
+	}
+	
+	@RequestMapping("/addProgrammer")
+	public String addProgrammer(@RequestParam int pId, @RequestParam String pName, @RequestParam String pLang,
+			Model model)
+	{
+		model.addAttribute("pId", pId);
+		model.addAttribute("pName", pName);
+		model.addAttribute("pLang", pLang);
+		
+		return "ProgrammerInfo.html";
+	}
+}
