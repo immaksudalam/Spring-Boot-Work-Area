668f3cd3790d994e27e64ab96c4eee868efc88e4
immaksudalam <maksudalamrony56@gmail.com>
2022-04-02 06:36:47 -0700

    folder added

---
+++ Get Mapping & Post Mapping/src/main/java/com/example/demo/model/Programmer.java
@@ -0,0 +1,57 @@
+package com.example.demo.model;
+
+public class Programmer {
+
+	private int pId;
+	private String pName;
+	private String pLang;
+	
+	
+	public Programmer() {
+		super();
+	}
+
+
+	public Programmer(int pId, String pName, String pLang) {
+		super();
+		this.pId = pId;
+		this.pName = pName;
+		this.pLang = pLang;
+	}
+
+
+	public int getpId() {
+		return pId;
+	}
+
+
+	public void setpId(int pId) {
+		this.pId = pId;
+	}
+
+
+	public String getpName() {
+		return pName;
+	}
+
+
+	public void setpName(String pName) {
+		this.pName = pName;
+	}
+
+
+	public String getpLang() {
+		return pLang;
+	}
+
+
+	public void setpLang(String pLang) {
+		this.pLang = pLang;
+	}
+	
+	@Override
+	public String toString() {
+		return "Programmer [pId=" + pId + ", pName" + pName + ", pLang" + pLang +"]";
+	}
+	
+}
