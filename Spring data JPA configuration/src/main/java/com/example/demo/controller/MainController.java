fbf86a0e53a51f9e9405498fccaa0424730c6d89
immaksudalam <maksudalamrony56@gmail.com>
2022-04-02 07:13:12 -0700

    folder added

---
+++ Spring data JPA configuration/src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,53 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.model.Programmer;
+import com.example.demo.repository.ProgrammerRepo;
+
+import antlr.collections.List;
+
+@Controller
+public class MainController {
+
+	@Autowired
+	ProgrammerRepo pr;
+	
+	@GetMapping("/home")
+	public String homePage()
+	{
+		return "HomePage.html";
+	}
+	
+	@PostMapping("/addProgrammer")
+	public ModelAndView addProgrammer(@ModelAttribute Programmer programmer)
+	{
+		pr.save(programmer);
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("ProgrammerInfo.html");
+		return mv;
+	}
+	
+	@GetMapping("/allProgrammer")
+	public String allProgrammer(Model m)
+	{
+		List p = (List) new ArrayList<Programmer>();
+		p.add(new Programmer(101, "maksud alam", "C++"));
+		p.add(new Programmer(102, "Jakariya Sakil", "PHP"));
+		p.add(new Programmer(101, "Ashikur Rahman", "JavaScript"));
+		
+		m.addAttribute("Programmers", p);
+		return "allProgrammer.html";
+	}
+}
